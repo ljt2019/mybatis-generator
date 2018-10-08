d45d34c10d380e10e2499f44ed241b85b8462b89
linjitai <1165069099@qq.com>
2018-10-08 17:09:33 +0800

    init

---
+++ src/main/java/com/mybatis/generator/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mybatis.generator.mapper;
+
+import com.mybatis.generator.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
